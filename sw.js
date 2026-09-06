8174001d79b1b48ec8597a1b2861e16350b8ec53
vernonmoreland123-sys <vernonmoreland123@gmail.com>
2026-09-06 08:51:00 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='room-command-center-v7-pwa-1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(fetch(e.request).then(r=>{
+    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+  }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
